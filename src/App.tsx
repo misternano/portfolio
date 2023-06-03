263072757912306b0188d78effc81b25eb8a0a09
misternano <cjstitt@icloud.com>
2023-06-03 00:04:54 -0400

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { ParallaxProvider } from "react-scroll-parallax";
-import Home from "./pages/Home.js";
-import Terms from "./pages/Terms.js";
-import Privacy from "./pages/Privacy.js";
+import { Home, Terms, Privacy } from "./pages";
 
 const App = () => {
 	return (
---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+import Home from "./Home.tsx";
+import Terms from "./Terms.tsx";
+import Privacy from "./Privacy.tsx";
+
+export {
+	Home,
+	Terms,
+	Privacy
+};
