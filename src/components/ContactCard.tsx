db6a7f8a6aceea1737c67cf03cf41f86dc92b8e2
misternano <cjstitt@icloud.com>
2023-06-07 18:37:26 -0400

    got rid of unnecessary variable

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-const submitForm = "https://getform.io/f/adff09e7-4050-40d8-9665-9ae955e8fb66";
-
 const ContactCard = () => {
 	return (
-		<form action={submitForm} method="POST" target="norel noopen" className="flex flex-col gap-3">
+		<form action="https://getform.io/f/adff09e7-4050-40d8-9665-9ae955e8fb66" method="POST" target="norel noopen" className="flex flex-col gap-3">
 			<div className="grid grid-cols-2 gap-3">
 				<input id="email" name="email" type="email" placeholder="Email *" className="bg-neutral-700/50 rounded-t border-b p-2 border-brand focus:rounded" required />
 				<input id="subject" name="subject" type="text" placeholder="Subject" className="bg-neutral-700/50 rounded-t border-b p-2 border-brand focus:rounded" />
