56cfccb0bb23552c1818696d793b935b1f1768e5
misternano <cjstitt@icloud.com>
2023-07-02 10:34:50 -0400

    improved tech stack carousel

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -3,7 +3,7 @@ import { Tech } from "../types";
 
 const TechCard: React.FC<{ tech: Tech }> = ({ tech }) => {
 	return (
-		<div className="-z-10 min-w-fit p-2 md:p-4 flex flex-col gap-2 justify-center items-center bg-smallcard border border-neutral-700 rounded-xl glow hover:border-neutral-500 transition-colors duration-200">
+		<div className="tech -z-10 min-w-fit p-2 md:p-4 flex flex-col gap-2 justify-center items-center bg-smallcard border border-neutral-700 rounded-xl glow hover:border-neutral-500 transition-colors duration-200">
 			<div className="flex flex-row gap-2 items-center">
 				<img src={`../assets/svg/${tech.image}.svg`} alt={`${tech.name} Logo`} className="w-6 h-auto aspect-square rounded" />
 				<p className="text-xl font-semibold">{tech.name}</p>
