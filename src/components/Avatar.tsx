5a6b66742abd72f008e3b4431f02f27dfc74bf28
misternano <cjstitt@icloud.com>
2023-07-24 19:46:02 -0400

    avatar status preventing page load when offline

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -36,11 +36,11 @@ const Avatar = () => {
 		<>
 			<div className="relative md:block hidden">
 				<img src={data ? `https://cdn.discordapp.com/avatars/272535850200596480/${data?.discord_user.avatar}.webp` : placeholder} alt="Avatar" className={`relative ${borderColor} w-52 h-auto backdrop-blur-xl bg-neutral-800/50 rounded-full border-2 z-20`} />
-				{data?.activities[0].id === "custom" && (
+				{data?.activities && data.activities.length > 0 ? data.activities[0].id === "custom" && (
 					<div className={`absolute bottom-3 right-3 z-20 w-10 h-10 flex justify-center items-center ${backgroundColor} backdrop-blur-sm border-2 ${borderColor} rounded-full`}>
 						<img className="w-7 h-7" src={`https://cdn.discordapp.com/emojis/${data?.activities[0].emoji?.id}.webp`} alt={`${data?.activities[0].emoji?.name} emoji`} />
 					</div>
-				)}
+				) : null}
 			</div>
 			<svg className="absolute z-10 -top-10 md:block hidden" xmlns="http://www.w3.org/2000/svg" width="550" height="300" preserveAspectRatio="xMidYMid" viewBox="0 0 1470 726">
 				<g transform="translate(735,363) scale(1,1) translate(-735,-363)">
