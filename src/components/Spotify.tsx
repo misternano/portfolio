9de231c241f76018b32143f43b1cbbf83bad97c7
misternano <cjstitt@icloud.com>
2023-12-25 22:14:42 -0500

    Add tooltip feature to Spotify component

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -1,6 +1,7 @@
 import placeholder from "../assets/images/album.png";
 import { useEffect, useState } from "react";
 import { Circle } from "lucide-react";
+import tippy from "tippy.js";
 
 interface SpotifyProps {
 	listening_to_spotify: boolean;
@@ -37,20 +38,28 @@ const Spotify = () => {
 	if (!data || !data?.listening_to_spotify)
 		return null;
 
+	const song = data.spotify?.song.replace(/ *\([^)]*\)/g, "").replace(/ *\[[^\]]*]/g, "").replace(/ - [Rr]ecorded [Aa]t.*/g, "");
+	const artist = data.spotify?.artist.replace(/;/g, " •");
+
+	tippy("#spotify", {
+		content: `Listen to ${song} on Spotify`,
+		placement: "bottom"
+	});
+
 	return (
 		<div className="fixed md:w-80 top-2 md:top-1 left-2 right-2 md:right-0 z-40">
 			<div className="w-fit mx-auto mb-1 px-2 flex flex-row items-center gap-1 backdrop-blur-sm rounded-full">
 				<p className="text-xs">Listening to Spotify</p> <Circle size="8" className="stroke-spotify fill-spotify animate-pulse" />
 			</div>
-			<a href={`https://open.spotify.com/track/${data.spotify?.track_id}`} target="norel noopen" className="group p-2 flex flex-row gap-2 bg-neutral-800/50 border border-neutral-700 hover:border-spotify backdrop-blur-sm rounded-xl">
-				<img className="h-16 w-16 rounded-md" src={data.spotify?.album_art_url ? data.spotify?.album_art_url : placeholder} alt={`${data.spotify?.album} Album Cover`} title={data.spotify?.album} />
+			<a href={`https://open.spotify.com/track/${data.spotify?.track_id}`} id="spotify" target="norel noopen" className="group p-2 flex flex-row gap-2 bg-neutral-800/50 border border-neutral-700 hover:border-spotify backdrop-blur-sm rounded-xl transition-colors">
+				<img className="h-16 w-16 rounded-md" src={data.spotify?.album_art_url ? data.spotify?.album_art_url : placeholder} alt={`${data.spotify?.album} Album Cover`} />
 				<div className="w-full truncate flex flex-col justify-between">
 					<div className="h-full w-full flex flex-col justify-center">
-						<p className="truncate text-white" title={data.spotify?.song.replace(/ *\([^)]*\)/g, "").replace(/ *\[[^\]]*]/g, "").replace(/ - [Rr]ecorded [Aa]t.*/g, "")}>
-							{data.spotify?.song.replace(/ *\([^)]*\)/g, "").replace(/ *\[[^\]]*]/g, "").replace(/ - [Rr]ecorded [Aa]t.*/g, "")}
+						<p className="truncate text-white">
+							{song}
 						</p>
-						<p className="truncate text-neutral-400" title={"by " + data.spotify?.artist.replace(/;/g, ',')}>
-							by {data.spotify?.artist.replace(/;/g, ' •')}
+						<p className="truncate text-neutral-400">
+							by {artist}
 						</p>
 					</div>
 				</div>
