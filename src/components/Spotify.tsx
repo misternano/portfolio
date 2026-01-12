5ddafdc40394b2129be3d14d1d26662705f7e9dc
Nano <cjstitt@icloud.com>
2026-01-12 12:00:55 -0500

    feat: spotify widget

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -1,69 +1,90 @@
-import placeholder from "../assets/images/album.png";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Circle } from "lucide-react";
-import tippy from "tippy.js";
+import { Progress } from "@/components/ui/progress";
+
+const formatTime = (ms: number) => {
+	const minutes = Math.floor(ms / 1000 / 60);
+	const seconds = Math.floor((ms / 1000 / 60) % 1 * 60);
+	
+	return minutes + ":" + seconds.toString().padStart(2, "0");
+};
 
 interface SpotifyProps {
-	listening_to_spotify: boolean;
-	spotify: {
-		track_id: string;
-		album_art_url: string;
-		artist: string;
-		song: string;
-		album: string;
+	is_playing: boolean;
+	timestamp: Date;
+	progress_ms: number;
+	item: {
+		id: string;
+		name: string;
+		duration_ms: number;
+		album: {
+			name: string;
+			images: {
+				url: string;
+			}[]
+		}
+		artists: {
+			name: string;
+		}[]
 	}
 }
 
 const Spotify = () => {
-	const [data, setData] = useState<SpotifyProps | null>(null);
-
-	const fetchData = async () => {
-		const response = await fetch("https://api.lanyard.rest/v1/users/272535850200596480");
-		const body = await response.json();
-		setData(body.data);
-	};
-
+	const [data, setData] = useState<SpotifyProps | undefined>(undefined);
+	
 	useEffect(() => {
-		fetchData();
 		const interval = setInterval((async () => {
 			try {
-				await fetchData();
+				const response = await fetch("https://api.ncc.dev/spotify");
+				const json = await response.json();
+				setData(json);
 			} catch (err) {
 				console.error(err);
 			}
-		}), 10000);
+		}), 2000);
+		
 		return () => clearInterval(interval);
 	}, []);
-
-	if (!data || !data?.listening_to_spotify)
-		return null;
-
-	const song = data.spotify?.song.replace(/ *\([^)]*\)/g, "").replace(/ *\[[^\]]*]/g, "").replace(/ - [Rr]ecorded [Aa]t.*/g, "");
-	const artist = data.spotify?.artist.replace(/;/g, " •");
-
-	tippy("#spotify", {
-		content: `Listen to ${song} on Spotify`,
-		placement: "bottom"
-	});
-
+	
+	if (!data || !data.is_playing) return null;
+	
+	const song = data.item.name.replace(/ *\([^)]*\)/g, "").replace(/ *\[[^\]]*]/g, "").replace(/ - [Rr]ecorded [Aa]t.*/g, "");
+	const artists = data.item.artists.map(a => a.name).join(" • ");
+	const bar = Math.floor(data.progress_ms / data.item.duration_ms * 100);
+	
 	return (
-		<div className="fixed md:w-80 top-2 md:top-1 left-2 right-2 md:right-0 z-40">
-			<div className="w-fit mx-auto mb-1 px-2 flex flex-row items-center gap-1 backdrop-blur-sm rounded-full">
-				<p className="text-xs">Listening to Spotify</p> <Circle size="8" className="stroke-spotify fill-spotify animate-pulse" />
+		<div className="md:w-fit absolute right-2 left-2 top-1">
+			<div className="mb-1 px-2 flex flex-row items-center justify-center gap-1 backdrop-blur-sm rounded-full">
+				<p className="text-xs font-[archia]">Listening to Spotify</p> <Circle size="8" className="stroke-[#1fdf64] fill-[#1fdf64] animate-pulse" />
 			</div>
-			<a href={`https://open.spotify.com/track/${data.spotify?.track_id}`} id="spotify" target="norel noopen" className="group p-2 flex flex-row gap-2 bg-neutral-800/50 border border-neutral-700 hover:border-spotify backdrop-blur-sm rounded-xl transition-colors">
-				<img className="h-16 w-16 rounded-md" src={data.spotify?.album_art_url ? data.spotify?.album_art_url : placeholder} alt={`${data.spotify?.album} Album Cover`} />
-				<div className="w-full truncate flex flex-col justify-between">
-					<div className="h-full w-full flex flex-col justify-center">
-						<p className="truncate text-white">
-							{song}
-						</p>
-						<p className="truncate text-neutral-400">
-							by {artist}
-						</p>
+			<div className="group md:w-96 backdrop-blur-sm ring-1 ring-border hover:ring-[#1fdf64]/75 bg-background rounded-lg transition-all">
+				<a href={`https://open.spotify.com/track/${data.item.id}`} target="norel noopen">
+					<div className="relative p-1.5 flex flex-row gap-2">
+						<img className="h-20 w-20 rounded-[6px]" src={data.item.album.images[0].url} alt="Album Cover" title={data.item.album.name} />
+						<div className="w-full flex flex-col justify-between">
+							<div className="h-full flex flex-col justify-center">
+								<p className="w-64 whitespace-nowrap text-ellipsis overflow-hidden text-primary" title={song}>
+									{song}
+								</p>
+								<p className="w-64 whitespace-nowrap text-ellipsis overflow-hidden text-secondary-foreground/50" title={artists}>
+									{artists}
+								</p>
+							</div>
+							<div>
+								<div className="flex justify-between">
+								<span id="timestamp">
+									{formatTime(data.progress_ms)}
+								</span>
+									<span id="timestamp">
+									{formatTime(data.item.duration_ms)}
+								</span>
+								</div>
+								<Progress value={bar} className="h-1" />
+							</div>
+						</div>
 					</div>
-				</div>
-			</a>
+				</a>
+			</div>
 		</div>
 	);
 };
